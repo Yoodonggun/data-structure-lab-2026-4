cf450e634bc4cfe5a4ab705503885e462de9a79e
Yoodonggun <a22278109@gmail.com>
2026-05-15 09:46:14 +0900

    5.15

---
+++ Lec10_Graph/Lec10_Graph/main.cpp
@@ -0,0 +1,20 @@
+% include "AdjMatGraph.h"
+void main)()
+{
+	AdjMatGraph g;
+
+	for (int i = 0; i < 4; i++)
+	g.insertVertex('A' + i);
+	g.insertEdge(0, 1);
+	g.insertEdge(0, 3);
+	g.insertEdge(1, 2);
+	g.insertEdge(1, 3);
+	g.insertEdge(2, 3);
+	printf("인접 행렬로 표현한그래프\n");
+	g.display();
+
+	FILE* fp;
+	fopen_s(&fp, "../text_out.txt", "\n");
+		g.display(fp);
+	fclose(fp);
+}
